d87f6ba84414ba6eb8681339f7e6bffd492d41a3
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-10-16 00:50:02 +0700

    feat: try tailwind

--- src/pages/api/og-image.tsx
+++ src/pages/api/og-image.tsx
@@ -1,13 +1,29 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
 import { withOGImage } from "next-api-og-image";
 
 export default withOGImage({
   template: {
     react: ({ heading, text }) => (
-      <Box padding={6}>
-        <Heading color="orange.600">{heading}</Heading>
-        <Text>{text}</Text>
-      </Box>
+      <>
+        <head>
+          <link
+            href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+            rel="stylesheet"
+          />
+          <link
+            href="https://unpkg.com/@fontsource/lexend@4.5.1/latin.css"
+            rel="stylesheet"
+          />
+        </head>
+        <div
+          className="min-h-screen bg-gray-100 p-12 flex flex-col justify-center gap-4"
+          style={{ fontFamily: "Lexend" }}
+        >
+          <h1 className="text-[3rem] font-bold text-blue-400 leading-tight">
+            {heading}
+          </h1>
+          <p className="font-medium text-[1.5rem] text-gray-500">{text}</p>
+        </div>
+      </>
     ),
   },
 });
