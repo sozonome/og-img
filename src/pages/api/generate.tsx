b3051d8c4053e7e4c36e27db8a2a3514fb4eb8f5
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-02-12 20:35:38 +0700

    chore: update packages to latest (#2)

--- src/pages/api/generate.tsx
+++ src/pages/api/generate.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-head-element */
 // https://play.tailwindcss.com/TYb0XtCyeG?size=1100x720
 
 import { withOGImage } from "next-api-og-image";
@@ -12,13 +13,13 @@ export default withOGImage({
             rel="stylesheet"
           />
           <link
-            href="https://unpkg.com/@fontsource/lexend@4.5.1/latin.css"
+            href="https://unpkg.com/@fontsource/outfit@4.5.4/latin.css"
             rel="stylesheet"
           />
         </head>
         <div
           className="min-h-screen bg-black p-12 flex flex-col justify-center gap-4"
-          style={{ fontFamily: "Lexend" }}
+          style={{ fontFamily: "Outfit" }}
         >
           <h1 className="text-6xl font-bold text-gray-300 leading-tight">
             {heading}
