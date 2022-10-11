06b5d883afc6f4583c8ac233bb157b32f1c4427d
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-10-11 17:07:25 +0700

    feat: switch next-api-og-image with @vercel/og (#27)

--- src/lib/constants/baseUrl.ts
+++ src/lib/constants/baseUrl.ts
@@ -1,1 +1,1 @@
-export const baseUrl = `https://og.sznm.dev`;
+export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "";
---
+++ src/lib/utils/font/outfit.ts
@@ -0,0 +1,8 @@
+import { fontLoader } from "./fontLoader";
+
+const outfitFontLoader = (weight: string) =>
+  fontLoader(`/assets/fonts/Outfit-${weight}.ttf`);
+
+export const outfitRegular = outfitFontLoader("Regular");
+export const outfitMedium = outfitFontLoader("Medium");
+export const outfitBold = outfitFontLoader("Bold");
