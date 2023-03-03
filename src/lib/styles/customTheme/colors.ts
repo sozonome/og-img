17cd01511b08bcbab8b936a6ca61b8d60952e873
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-03-04 00:51:50 +0700

    refactor(theme): adjust gray color palette

--- src/lib/styles/customTheme/colors.ts
+++ src/lib/styles/customTheme/colors.ts
@@ -18,7 +18,20 @@ const extendedColors: DeepPartial<
 };
 
 /** override chakra colors here */
-const overridenChakraColors: DeepPartial<Theme["colors"]> = {};
+const overridenChakraColors: DeepPartial<Theme["colors"]> = {
+  gray: {
+    50: "#e8e9e9",
+    100: "#d1d2d2",
+    200: "#a3a5a5",
+    300: "#747978",
+    400: "#464c4b",
+    500: "#181f1e",
+    600: "#131918",
+    700: "#0e1312",
+    800: "#0a0c0c",
+    900: "#050606",
+  },
+};
 
 export const colors = {
   ...overridenChakraColors,
